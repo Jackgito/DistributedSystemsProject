a7db70761f283ee53714e476dc07166c381c813e
emmampakarinen <emma.m.pakarinen@gmail.com>
2024-04-24 16:05:04 +0300

    create nodejs server and make required changes to python server and client

--- client/helper_functions.py
+++ client/helper_functions.py
@@ -5,14 +5,14 @@ def clear_screen():
 
 def print_posts(array_of_posts):
   for post in array_of_posts:
-    hashtags_formatted = ' '.join(f'#{tag}' for tag in post['hashtags'])
+    hashtags_formatted = ' '.join(f'#{tag}' for tag in post['Hashtags'])
     comments_formatted = '\n'.join(f" {comment['Username']}: {comment['Text']}" for comment in post['Comments'])
 
     print(
         f''' 
         |{post['Poster']}: {post['Title']}| {post['Timestamp']}
 
-        {post['text']}
+        {post['Text']}
 
         Likes: {post['Likes']} | Hashtags: {hashtags_formatted}
         ----------------------------
--- server/server.py
+++ server/server.py
@@ -1,20 +1,23 @@
-import bcrypt # pip3 install bcrypt (used for password hashing)
-import pymongo # pip3 install pymongo (MongoDB is database of choice)
+import requests # pip3 install requests 
 from xmlrpc.server import SimpleXMLRPCServer
 from datetime import datetime
-DBCLIENT = pymongo.MongoClient("mongodb://localhost:27017")
-DBCLIENT.server_info()
-print("Connection to MongoDB established successfully!")
-DB = DBCLIENT["DSproject"]
-COLLECTION = DB["users"] # users is cluster name (cluster is like a table)
-POSTS = DB["Posts"]
+
 
 # Used for sign up. Returns true if username exits, false otherwise
 def is_username_unique(username):
-  user_count = COLLECTION.count_documents({"username": username})
-  return user_count == 0
 
-def go_through_posts( cursor):
+  url = 'http://localhost:5000/is_username_unique' 
+  params = {"username": username}
+  try: 
+    response = requests.get(url, params=params) 
+    data = response.json()
+
+    return data['isUnique']
+  except requests.exceptions as e:
+     print(f"Request failed: {e}")
+  return None
+
+def go_through_posts(cursor):
   result_array=[]
   for post in cursor:
     result_array.append(post)
@@ -23,46 +26,50 @@ def go_through_posts( cursor):
      i['Timestamp'] = str(i['Timestamp'])
   return result_array
 
-def create_user(username, password):
-  # Hash the password using bcrypt
-  hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
-  
-  # Save username and hashed password to the database
-  try:
-    data = {"username": username, "password": hashed_password.decode('utf-8')}
-    COLLECTION.insert_one(data)
-  except:
-    return False
-  
-  return True
+def create_user(username, password): 
 
-def login(username, password):
-    # Find user by username
-    user = COLLECTION.find_one({"username": username})
+  # calling server to insert the user into the database
+  data = {"username": username, "password": password}
+  url = 'http://localhost:5000/create_user' 
 
-    # Check if username exists
-    if user is None:
-        print("Invalid username or password")
-        return False
+  response = requests.post(url, json=data) 
 
-    # Verify password using bcrypt
-    hashed_password = user["password"].encode('utf-8')  # Convert stored password to bytes
-
-    if not bcrypt.checkpw(password.encode('utf-8'), hashed_password):
-        print("Invalid username or password")
-        return False
-
-    # Login successful
-    print(f"Welcome back, {username}!")
+  if (response.status_code == 201):
     return True
+  else:
+    return False
 
-def is_title_unique(title):
-   if (POSTS.find_one({"Title": title})):
-       return False
-   
-   return True
 
-def create_post(poster, title, postText, timestamp, hashtags):
+def login(username, password): 
+    
+    url = 'http://localhost:5000/login'
+    data = {'username': username, 'password': password}
+    response = requests.post(url, json=data)
+
+    if response.status_code == 200:
+        print("Login successful")
+        return True
+    elif response.status_code == 404:
+        print("User not found")
+        return False
+    elif response.status_code == 401:
+        print("Invalid credentials")
+        return False
+    else:
+        print("Failed to authenticate")
+        return False
+    
+
+def is_title_unique(title): 
+  url = 'http://localhost:5000/is_title_unique' 
+  params = {'title': title}
+
+  response = requests.get(url, params=params)
+  data = response.json()
+
+  return data['isUnique']
+
+def create_post(poster, title, postText, timestamp, hashtags): 
   '''
   Creates new post and adds it to database. Parameters:
   - poster (str): the user who created the post
@@ -71,68 +78,70 @@ def create_post(poster, title, postText, timestamp, hashtags):
   - timestamp (datetime): time when post was created
   - hashtags (list): list of hashtags associated with the post
   '''
-  try:
-    data = {"Title": title, "Poster": poster, "text": postText, "Timestamp": timestamp, "hashtags": hashtags, "Likes": 0, "Comments": []}
-    POSTS.insert_one(data)
-  except:
+  url = 'http://localhost:5000/create_post' 
+  data = {"title": title, "poster": poster, "text": postText, "timestamp": timestamp, "hashtags": hashtags}
+  
+  response = requests.post(url, json=data) 
+  print(response.status_code)
+  
+  if (response.status_code == 201):
+    return True
+  else:
     return False
   
-  return True
 
 # Fetch 10 newest posts
-def fetch_posts():
+def fetch_posts(): 
   # data = {"Title": "Title for post", "Poster": "username", "text":"Text for the post", "Timestamp":datetime.now(), "hashtags":["#hastag1", "#hastag2"], "Likes": 10, "Comments":["commenttext1"]}
-  # POSTS.insert_one(data)
-  posts =POSTS.find().sort([('Timestamp', -1)]).limit(10)
+  url = 'http://localhost:5000/posts' 
+  response = requests.get(url) 
   
+  if response.status_code == 200:
+        posts = response.json() 
+        return go_through_posts(posts)
+  else:
+      print("Error occurred when retrieving posts:", response.status_code)
+      return None
+
+def find_hashtag(hashtag): 
+  url = 'http://localhost:5000/posts_hashtag' 
+  params = {"hashtag": hashtag}
+  response = requests.get(url, params=params) 
   
-  return go_through_posts(posts)
+  if response.status_code == 200:
+        posts = response.json() 
+        return go_through_posts(posts)
+  else:
+      print("Error occurred when retrieving posts with hashtag:", response.status_code)
+      return None
 
-def find_hashtag(hashtag):
-  posts = POSTS.find({"hashtags" : {"$in" : [hashtag]}})
-  return go_through_posts(posts)
+def like_post(post_name, username): 
+  url = 'http://localhost:5000/like'
+  data = {'title': post_name, 'username': username}
+  response = requests.post(url, json=data)
 
-def like_post(post_name, username, comment):
-  try:
-
-    # Check if post exists
-    if not POSTS.find_one({"Title": post_name}):
-      return "PostNotFound"
-    
-    # Check if user has liked already. 
-    # If they have, remove the like and user from the liked list
-    if POSTS.find_one({"Title": post_name, "LikedUsers": {"$in": [username]}}):
-      POSTS.find_one_and_update(
-          {"Title": post_name, "LikedUsers": {"$in": [username]}},
-          {"$pull": {"LikedUsers": username}, "$inc": {"Likes": -1}},
-      )
-      return "LikeRemoved"
-    else:
-      # User hasn't liked, so add like and user
-      POSTS.find_one_and_update(
-          {"Title": post_name},
-          {"$inc": {"Likes": 1}, "$addToSet": {"LikedUsers": username}},
-      )
+  if response.status_code == 201:
       return "LikeAdded"
-
-  except:
-    return "Error"
+  elif response.status_code == 404:
+      return "PostNotFound"
+  elif response.status_code == 204:
+      return "LikeRemoved"
+  else:
+      return "Error"
+  
 
 def comment_post(post_name, username, comment):
-  try:
-    # Check if post exists
-    if not POSTS.find_one({"Title": post_name}):
+  url = 'http://localhost:5000/comment'
+  data = {'title': post_name, 'username': username, 'comment': comment}
+  response = requests.post(url, json=data)
+
+  if response.status_code == 201:
+      return "CommentAdded"
+  elif response.status_code == 404:
       return "PostNotFound"
-
-    # Update the post with the new comment
-    POSTS.find_one_and_update(
-        {"Title": post_name},
-        {"$push": {"Comments": {"Username": username, "Text": comment}}},
-    )
-    return "CommentAdded"
-
-  except:
-    return "Error"
+  else:
+      return "Error"
+  
 
 
 if __name__ == "__main__":
